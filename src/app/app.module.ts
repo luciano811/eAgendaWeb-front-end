f5669929fa0309eacd755db7e6c4d1c70d81052b
Luciano Dalmollin <xlucianox811@gmail.com>
2022-10-07 15:29:46 -0300

    feat: configurações de imports do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarModule } from './navbar/navbar.module';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/services/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    //aqui em baixo tive que add esse forRoot aqui se
+    //nao a navbar nao aparecia de jeito nenhum
+    RouterModule.forRoot(
+      [
+        { path: "", component: NavbarModule}
+      ]
+    ),
+
+    AuthModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
